f52b47d660c877c5c3d67db05c4353ee3a64b372
Bhawesh Choudhary <bhaweshchoudhary100@gmail.com>
2025-12-26 02:10:06 +0530

    Variables, DataTypes (Primitives & Compled)

---
+++ FrontEndBasics/JsBasics/Day1/script.js
@@ -0,0 +1,30 @@
+/* 
+================ Variables ============
+    - Named storage location whose values can be manupulated during execution of the program.
+    - How to make them ?
+        -- use: var, let (can be reassigned) , const (can't be reassigned)
+        -- randomGuy = "simpson"; ()
+    - Note: In javascript, whenever it executes, all files gets combined to make a object called as "Window"
+    - Window (object) is a Global Variable & Predifined
+    - Local Scope (Function/Parent Scope) & Global Scope.
+    - when we use 'let' & 'const' variables it creates a 'block level scope' (technically its not a scope)
+
+
+================= Data Types ==========
+    - Types : Primitives & Non-Primitives (Complex Primitives)
+    
+        - # Primitive
+            -- undefined (default value)
+            -- null (explicit value: sometimes treated as default value and sometimes as value)
+            -- Number
+            -- BigInt
+            -- Symbol (a way to secure)
+            -- String
+            -- Boolean
+        
+        - # Complex Primitives
+            -- Objects (stored in key value pair)
+                -- objects works on refrences.
+
+
+*/
