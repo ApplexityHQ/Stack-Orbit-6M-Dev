7db7cf4a2a3c6e30af4a5abf976b05777169bb0d
Bhawesh Choudhary <bhaweshchoudhary100@gmail.com>
2026-01-01 21:06:57 +0530

    Flexbox

---
+++ FrontEndBasics/JsBasics/Day2/script.js
@@ -0,0 +1,28 @@
+/**
+ * =========== Statically Typed & Dynamically Typed Languages ================
+ * 
+ *      - Statically Typed:
+ *          -- 
+ *      - Dynamically Typed: 
+ *          -- 
+ * 
+ * ============== Type Conversion / Typecasting =================
+ *      - 
+ * 
+ *      . (Dot keyword) is used for two things (1. for Key , 2. For Decimal)
+ * 
+ *  
+ *      - falsy values : NaN, 0, empty strings, false, undefined, null
+ * 
+ * 
+ * =================== Closure Basics ======================
+ *      - A function with some preserved values creates a closure.
+ *      - A closure is created when a function preserves variables outside of its scope. 
+ * 
+ * ================== Curring ======================
+ *      - a function returing a function.
+ * 
+ * 
+ * 
+ * 
+ */
